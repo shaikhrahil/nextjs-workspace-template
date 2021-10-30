5518d30d87bb4c8759cfae74026901fd719b6039
shaikhrahil <smohammadrahil@gmail.com>
2021-10-30 23:51:42 +0530

    ✨ init

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  purge: ["pages/**/*.tsx"],
+  darkMode: "class", // or 'media' or 'class'
+  theme: {
+    extend: {
+      primary: "",
+      "primary-muted": "",
+      surface: "",
+      "surface-muted": "",
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
